db8208472dae31da00cc230127b6c2905890b7ac
codeUWE <code.cayan@gmail.com>
2024-02-26 17:53:28 +0100

    first styling of the output

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import NewsBody from "./components/NewsBody"
+import Nav from "./components/Nav"
 
 import './App.css'
 
@@ -6,6 +7,7 @@ function App() {
   
   return (
     <>
+      <Nav />
       <NewsBody />
     </>
   )
