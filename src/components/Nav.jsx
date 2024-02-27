920f5e4b2b6a29ce9d08ceae5205ed6af4819379
codeUWE <code.cayan@gmail.com>
2024-02-27 17:09:16 +0100

    small changes in main

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,14 @@
 export default function Nav() {
     return (
         <nav className="navBar">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Latest News</a></li>
-            <li><a href="#">On Focus</a></li>
-            <li><a href="#">Login</a></li>
+            <li><a href="#">new</a></li>
+            <li><a href="#">past</a></li>
+            <li><a href="#">comments</a></li>
+            <li><a href="#">ask</a></li>
+            <li><a href="#">show</a></li>
+            <li><a href="#">jobs</a></li>
+            <li><a href="#">submit</a></li>
+            <li><a href="#">login</a></li>
         </nav>
     )
 }
