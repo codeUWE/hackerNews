072d936716e868b34bf070810bcc804d7df626df
codeUWE <code.cayan@gmail.com>
2024-02-27 17:42:39 +0100

    first work on nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,16 @@
 export default function Nav() {
     return (
         <nav className="navBar">
-            <li><a href="#">new</a></li>
-            <li><a href="#">past</a></li>
-            <li><a href="#">comments</a></li>
-            <li><a href="#">ask</a></li>
-            <li><a href="#">show</a></li>
-            <li><a href="#">jobs</a></li>
-            <li><a href="#">submit</a></li>
+            <div className="navArticle">
+                <p>hackerNews!</p>
+                <li><a href="#">new |</a></li>
+                <li><a href="#">past |</a></li>
+                <li><a href="#">comments |</a></li>
+                <li><a href="#">ask |</a></li>
+                <li><a href="#">show |</a></li>
+                <li><a href="#">jobs |</a></li>
+                <li><a href="#">submit </a></li>
+            </div>
             <li><a href="#">login</a></li>
         </nav>
     )
